8a2b9ed66fbe27845a77dd4c603b7ad79de41fd1
ankitkumar131 <ak3057955@gmail.com>
2025-11-22 20:06:16 +0530

    ui-label : ui label component is ready with full customization and detailed user manual

---
+++ projects/my-ui/label/label.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { LabelComponent } from './components/label/label.component';
+
+@NgModule({
+  imports: [LabelComponent],
+  exports: [LabelComponent]
+})
+export class UiLabelModule { }
